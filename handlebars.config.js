cfc579923c2de1e5a29cff65bc7a8f30cdc42f33
eggei <macitegeercan@gmail.com>
2022-03-11 14:00:21 -0500

    integrate handlebars

---
+++ handlebars.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  data: "src/views/data/**/*.{json,js}",
+  decorators: "src/views/decorators/**/*.js",
+  helpers: "src/views/helpers/**/*.js",
+  layouts: "src/views/layouts/**/*.{hbs,handlebars,js}",
+  partials: "src/views/partials/**/*.{hbs,handlebars,js}",
+};
